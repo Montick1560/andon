91bc1cfc871f5cf92b1b8cf8058369e10367dce5
Montick1560 <ponchesito5000@gmail.com>
2024-07-29 23:10:15 -0600

    Se está terminando el layout del sidebar

--- src/core/components/Main.jsx
+++ src/core/components/Main.jsx
@@ -3,7 +3,6 @@ import s from '../../css/main.module.css'
 export function Main(){
     return(
         <section className={s.mainContent}> 
-            <h1>SIUUUUUUUUUUU</h1>
         </section>
     )
 }
--- src/core/components/Sidebar.jsx
+++ src/core/components/Sidebar.jsx
@@ -4,13 +4,25 @@ import s from "../../css/sidebar.module.css";
 export function Sidebar() {
   return (
     <nav className={s.sideBarApp}>
-     <header className={s.headerTotal}>
-       <img className={s.imgInoac} src="public/img/inoac.png"></img>
-     </header>
-     <section className={s.relojTiempo}>
-     </section>
-     <section className={s.bottom}>
-     </section>
+      <header className={s.headerTotal}>
+        <img className={s.imgInoac} src="public/img/INOACLOGOw.png"></img>
+      </header>
+      <div className={s.barraAzul}></div>
+      <section className={s.moldeActual}>
+       <img className={s.imgBotonEstado} src='public/img/botonGris.png'></img> 
+       <h1>Esperando molde...</h1>
+      </section>
+      <section className={s.relojTiempo}>
+        <img className={s.imgReloj} src="public/img/reloj.png"></img>
+        <h1>10:26:26 p.m</h1>
+      </section>
+      <section className={s.bottom}>
+        <button className={s.buttonAjustes}>AJUSTES</button>
+      </section>
+      <div className={s.barraAzul}></div>
+      <section className={s.headerTotal}>
+        <img className={s.imgInoac} src="public/img/baacc.png"></img>
+      </section>
     </nav>
   );
 }
