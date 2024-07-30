35784d4b09fb83d6144a188ae400724fb10eb87f
Montick <edson.reyes@baacc.mx>
2024-07-30 16:02:37 -0600

    Se agregaron estados y effectos para entender la lógica de react

--- src/core/components/Sidebar.jsx
+++ src/core/components/Sidebar.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import s from "../../css/sidebar.module.css";
 
 export function Sidebar() {
+  const [currentTime, setCurrentTime] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const date = new Date();
+      const hours = String(date.getHours()).padStart(2, "0");
+      const minutes = String(date.getMinutes()).padStart(2, "0");
+      const seconds = String(date.getSeconds()).padStart(2, "0");
+
+      setCurrentTime(`${hours}:${minutes}:${seconds}`);
+    }, 1000);
+  }, [currentTime]);
+
   return (
     <nav className={s.sideBarApp}>
       <header className={s.headerTotal}>
         <img className={s.imgInoac} src="public/img/INOACLOGOw.png"></img>
       </header>
       <div className={s.barraAzul}></div>
-      <section className={s.moldeActual}>
-       <img className={s.imgBotonEstado} src='public/img/botonGris.png'></img> 
-       <h1>Esperando molde...</h1>
-      </section>
+      <Molde/>
       <section className={s.relojTiempo}>
         <img className={s.imgReloj} src="public/img/reloj.png"></img>
-        <h1>10:26:26 p.m</h1>
+        <h1>{currentTime}</h1>
       </section>
       <section className={s.bottom}>
         <button className={s.buttonAjustes}>AJUSTES</button>
@@ -26,3 +36,13 @@ export function Sidebar() {
     </nav>
   );
 }
+function Molde(){
+  const [statusMolde,setStatusMolde] = useState(null)
+  return(
+    <section className={s.moldeActual}>
+    <img className={s.imgBotonEstado} src={statusMolde == 1 ? "public/img/botonGris.png": statusMolde == 2 ?"public/img/botonVerde.png":"public/img/botonRojo.png" }></img>
+    <h1>Esperando molde...</h1>
+  </section>
+  )
+}
+
